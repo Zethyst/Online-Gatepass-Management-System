5cce5ed5e35099c8c8cb894c32b16e038227c906
Ekansh <zethyst@protonmail.com>
2024-05-31 04:12:28 +0530

    Added Comments

--- connection.js
+++ connection.js
@@ -3,7 +3,7 @@ const con = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database:"gatepass"
+    database:"gatepass" //If no database found error occurs for first time use, remove this and add later
 });
 
 module.exports=con;
